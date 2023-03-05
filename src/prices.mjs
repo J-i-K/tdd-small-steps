b6d5249d5a2e24da680cad722c19e5441d525e1d
J-i-K <J-i-K@users.noreply.github.com>
2023-03-05 13:02:49 +0000

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -27,7 +27,7 @@ function createApp(database) {
   function parseDate(dateString) {
     if (dateString) {
       // console.log(new Date(dateString))
-      // console.log(Temporal.PlainDate.from(dateString))
+      console.log(Temporal.PlainDate.from(dateString))
       return new Date(dateString);
     }
   }
