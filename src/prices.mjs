211e4efe4653bb7c36586cea7e351142bfd7313e
J-i-K <J-i-K@users.noreply.github.com>
2023-03-05 13:51:03 +0000

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -90,7 +90,7 @@ function createApp(database) {
       if (
         date && date2 &&
         date2.year === holiday2.year &&
-        date.getMonth() === holiday.getMonth() &&
+        date2.month === holiday2.month &&
         date.getDate() === holiday.getDate()
       ) {
         return true;
