6ec3f82ee35628e8203935bb7cd133960fd898e8
J-i-K <J-i-K@users.noreply.github.com>
2023-03-05 13:57:53 +0000

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -18,7 +18,7 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
+    const date = parseDate2(req.query.date);
     const date2 = parseDate2(req.query.date)
     const cost = calculateCost(age, type, date2, baseCost);
     res.json({ cost });
