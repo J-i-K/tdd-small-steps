c515a5614268ab5613bfe348610cbda27e6b3311
J-i-K <J-i-K@users.noreply.github.com>
2023-03-06 08:47:17 +0000

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -79,7 +79,7 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.getDay() === 1;
+    return dateToTemporalPlainDate(date).dayOfWeek === 1;
   }
 
   function isHoliday(date) {
